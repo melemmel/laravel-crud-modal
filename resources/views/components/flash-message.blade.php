8df7d1fdfdf9507ae63f6fe4cfc860676971a7d5
Rommel Ramos <ramosrommel2001@gmail.com>
2023-12-30 13:01:11 +0800

    feat: redesign the layout of flash message

--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -1,8 +1,9 @@
-<div class="container py-2">
-    @if (session()->has('message'))
-        <div x-data="{ showMessage: true }" x-show="showMessage" class="alert alert-success fade show" role="alert"
-            x-init="setTimeout(() => showMessage = false, 4000)">
-            {{ session('message') }}
+@if (session()->has('message'))
+    <div x-data="{ showMessage: true }" x-show="showMessage"
+        class="alert alert-success fade show position-fixed bottom-0 end-0 p-3 text-center" role="alert"
+        x-init="setTimeout(() => showMessage = false, 3000)">
+        <div class="container">
+            <strong>Awesome!</strong> {{ session('message') }}
         </div>
-    @endif
-</div>
+    </div>
+@endif
