6b6316fb669220e92dd44ba58858acd30ad8bd38
Rommel Ramos <ramosrommel2001@gmail.com>
2023-12-30 14:53:13 +0800

    feat: update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -41,7 +41,7 @@
                                 <div class="mb-3">
                                     <label for="first_name" class="form-label">First Name</label>
                                     <input type="text" class="form-control @error('first_name') is-invalid @enderror"
-                                        id="first_name" name="first_name" value="{{ old('first_name') }}">
+                                        id="first_name" name="first_name" value="{{ old('first_name') }}" required>
                                     @error('first_name')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
@@ -52,7 +52,7 @@
                                 <div class="mb-3">
                                     <label for="last_name" class="form-label">Last Name</label>
                                     <input type="text" class="form-control @error('last_name') is-invalid @enderror"
-                                        id="last_name" name="last_name" value="{{ old('last_name') }}">
+                                        id="last_name" name="last_name" value="{{ old('last_name') }}" required>
                                     @error('last_name')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
@@ -63,7 +63,7 @@
                                 <div class="mb-3">
                                     <label for="address" class="form-label">Address</label>
                                     <textarea class="form-control @error('address') is-invalid @enderror" id="address" name="address" rows="3"
-                                    >{{ old('address') }}</textarea>
+                                        required>{{ old('address') }}</textarea>
                                     @error('address')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
@@ -74,7 +74,7 @@
                                 <div class="mb-3">
                                     <label for="age" class="form-label">Age</label>
                                     <input type="number" class="form-control @error('age') is-invalid @enderror"
-                                        id="age" name="age" value="{{ old('age') }}">
+                                        id="age" name="age" value="{{ old('age') }}" required>
                                     @error('age')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
@@ -85,7 +85,7 @@
                                 <div class="mb-3">
                                     <label for="department" class="form-label">Department</label>
                                     <input type="text" class="form-control @error('department') is-invalid @enderror"
-                                        id="department" name="department" value="{{ old('department') }}">
+                                        id="department" name="department" value="{{ old('department') }}" required>
                                     @error('department')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
